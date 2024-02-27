8b1322b266e1d4f269e5f09251b22cf873ae886b
donaditoUVG <don22684@uvg.edu.gt>
2024-02-26 18:11:32 -0600

    Cambio final al portal electrónico (Main)

--- Checkpoint/factory/PortalElectronico.java
+++ Checkpoint/factory/PortalElectronico.java
@@ -1,51 +1,39 @@
-/**
- * 
- */
 import java.util.Scanner;
 
-/**
- * @author MAAG
- *
- */
 public class PortalElectronico {
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
 
-    System.out.println("Bienvenido al portal electrónico");
-    System.out.println("Ingrese su usuario (correo electronico)");
-    String username = in.nextLine();
-    System.out.println("Ingrese la contrasenia");
-    String password = in.nextLine();
+        System.out.println("Bienvenido al portal electrónico");
+        System.out.println("Ingrese su usuario (correo electrónico):");
+        String username = in.nextLine();
+        System.out.println("Ingrese la contraseña:");
+        String password = in.nextLine();
 
-    Login myLogin = new Login();
-    UserInstanceCreator factory = new UserInstanceCreator();
+        Login myLogin = new Login();
+        UserInstanceCreator factory = new UserInstanceCreator();
 
-    if (myLogin.hasAccess(username, password)) {
-      User loggedUser = factory.getInstance(username);
-	  // Actualiza la información del usuario
+        if (myLogin.hasAccess(username, password)) {
+            User loggedUser = factory.getInstance(username);
+            loggedUser.setUsername(username);
+            loggedUser.setPassword(password);
+            loggedUser.printMenu();
 
-  		loggedUser.setUsername(username);
-  		loggedUser.setPassword(password);
-      loggedUser.printMenu();
+            // Guardar información del usuario al salir del menú
+            System.out.println("¿Desea guardar su información? (y/n)");
+            String saveChoice = in.nextLine();
 
-      // Guardar información del usuario
-      System.out.println("Desea guardar su información? (y/n)");
-      String saveChoice = in.nextLine();
+            if (saveChoice.equalsIgnoreCase("y")) {
+                System.out.println("Ingrese el nombre del archivo:");
+                String fileName = in.nextLine();
+                loggedUser.guardarInformacion(fileName);
+                System.out.println("¡Información guardada con éxito!");
+            }
+        } else {
+            System.out.println("Usuario o contraseña incorrectos");
+        }
 
-      if (saveChoice.equalsIgnoreCase("y")) {
-        System.out.println("Ingrese el nombre del archivo:");
-        String fileName = in.nextLine();
-        loggedUser.guardarInformacion(fileName);
-        System.out.println("Información guardada con éxito!");
-      }
-    } else {
-      System.out.println("Usuario o contrasenia incorrectos");
+        in.close();
     }
-
-    in.close();
-  }
 }
