316a5da54e90f017587b88c73f2e242b770092dc
donaditoUVG <don22684@uvg.edu.gt>
2024-03-11 21:23:34 -0600

    INFORME FINAL

--- HT5/CPU.py
+++ HT5/CPU.py
@@ -11,7 +11,7 @@ RANDOM_SEED = 42
 # Parámetros para configurar la simulación
 INTERVALOS = [10, 5, 1] ##aumenta la carga
 MEMORIA_TOTAL = 100 #RAM disponible
-CAPACIDAD_CPU = 1 #CPU's disponibles 
+CAPACIDAD_CPU = 2 #CPU's disponibles 
 VELOCIDAD_CPU = 3  # Instrucciones por cierta unidad de tiempo
 INSTRUCCIONES_MAX = 10 #max de instrucciones 
 TIEMPO_MAXIMO = 100
